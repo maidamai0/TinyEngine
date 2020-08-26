140d24840bdafc81185ce2217b8e81664953b30f
tonghao,yuan <yuantonghao@gmail.com>
2020-08-26 23:35:59 +0800

    feat: :sparkles: 3_automata

--- examples/3_Automata/main.cpp
+++ examples/3_Automata/main.cpp
@@ -1,32 +1,32 @@
-#include "../../TinyEngine.h"
+#include "model.h"
+#include "helpers/common.h"
+
 #include "../../include/helpers/image.h"
 #include "../../include/helpers/color.h"
-
-#include "model.h"
 
 int main( int argc, char* args[] ) {
 
 	Tiny::view.vsync = false;															//Turn off VSYNC before opening window
 
-	Tiny::window("GPU Accelerated Cellular Automata Example", WIDTH, HEIGHT);	//Open Window
+	Tiny::window("GPU Accelerated Cellular Automata Example", wind_width, wind_height);	//Open Window
 	Tiny::event.handler = eventHandler;										//Define Event Handler
 	Tiny::view.interface = [](){ /* ... */ }; 						//No Interface
 
 	setup();	//Setup Model Data
 
 	//Construct a billboard, using a texture generated from the raw data
-	Billboard targetA(image::make<bool>(glm::vec2(WIDTH, HEIGHT), data, [](bool t){
+	Billboard targetA(image::make<bool>(glm::vec2(wind_width, wind_height), data.data(), [](bool t){
 		if(t) return glm::vec4(1.0, 1.0, 1.0, 1.0);
 		else return glm::vec4(0.0, 0.0, 0.0, 1.0);
 	}));
-	Billboard targetB(WIDTH, HEIGHT, true, false); //color, no depth
+	Billboard targetB(wind_width, wind_height, true, false); //color, no depth
 	bool flip = true;
 
 	Square2D flat;	//Flat square primitive for drawing billboard to screen
 
 	//Shader for drawing billboard to screen and for doing an automata step
-	Shader shader({"shader/billboard.vs", "shader/billboard.fs"}, {"in_Quad", "in_Tex"});
-	Shader automata({"shader/automata.vs", "shader/automata.fs"}, {"in_Quad", "in_Tex"});
+	Shader shader({full_path("shader/billboard.vs").c_str(), full_path("shader/billboard.fs").c_str()}, {"in_Quad", "in_Tex"});
+	Shader automata({full_path("shader/automata.vs").c_str(), full_path("shader/automata.fs").c_str()}, {"in_Quad", "in_Tex"});
 
 	Tiny::view.pipeline = [&](){
 
--- examples/3_Automata/model.h
+++ examples/3_Automata/model.h
@@ -1,20 +1,26 @@
+#include <functional>
+#include <array>
+
+#include "../../TinyEngine.h"
+
 //Data
-const int SIZE = 100;
-const int WIDTH = 1200, HEIGHT = 800;
-bool data[WIDTH*HEIGHT] = {false};
+constexpr auto size = 100;
+constexpr auto wind_width = 1200;
+constexpr auto wind_height = 800;
+std::array<bool, wind_width*wind_height> data = {false};
 bool paused = true;
 
 //Setup of Data
 void setup(){
   //Generate Random Data!
-  srand(time(NULL));
-  for(int i = 0; i < WIDTH*HEIGHT; i++){
+  srand(time(nullptr));
+  for(int i = 0; i < wind_width*wind_height; i++){
     data[i] = (rand()%2 == 0)?1:0;
   }
 };
 
 // Event Handler
-std::function<void()> eventHandler = [&](){
+std::function<void()> eventHandler = [](){
 
   // Pause and Unpause
   if(!Tiny::event.press.empty())
